fd37ae1edefed2f69dce5fc1e3396b02782d6fa6
yiro0 <bartmat.p@gmail.com>
2025-01-26 12:22:47 +0100

    lab06: start
    lab06: start - pom.xml changes, removal of history script from html file

--- lab06/untitled/src/main/java/repository/HistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.History;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoryRepository extends JpaRepository<History, Long> {
-}
